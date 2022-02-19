d1e0b707ebd3d98ea5122c0961d9bbdad81dda98
YezGotIt <83292822+YezGotIt@users.noreply.github.com>
2022-02-19 20:37:48 +0530

    Add files via upload

---
+++ educational purpose/For YouTube Creators - partial feature/README.md
@@ -0,0 +1,54 @@
+# For YouTube Creators - partial feature (Educational Purpose)
+
+### See in [Instagram](https://www.instagram.com/tv/CXqQ0tLFFTR/) video
+
+### Watch this video (Tamil) 
+
+1. https://www.youtube.com/watch?v=BKks3jF3kWU
+
+2. https://www.youtube.com/watch?v=7c9eBJ2qAmY
+
+
+### It will be scraped YouTube channel page to collect the information such as the about page i.e(join date, description, total views, location,total subscribers) and video which post on your channel i.e(video link,views,isStreamed,relative time, duration).
+<br>
+
+#### REQUIREMENT:
+- Download [Python](https://python.org) latest version.
+- Install pip (Pip Installs Python)
+    - To install pip. Run this command `python get-pip.py` or `pip install pip` 
+<br>
+
+#### PROCEDURE:
+- Download the zip folder from Github and unzip it
+Here is the link to download zip 👉
+<a href='https://github.com/YezGotIt/source-code/raw/main/educational%20purpose/For YouTube Creators - partial feature/for%20youtube%20creators.zip'>HERE</a>
+- Open the folder in VS Code or 
+- RUN, this command pulls out all the require modules:
+
+- RUN, this command given below (Install the dependencies):
+```
+pip install -r requirements.txt
+```
+![run code](./screenshot/1.png)
+- RUN, this command given below (Run the app.py):
+```
+python app.py -l <channel link>
+```
+![run code](./screenshot/2.png)
+
+<br>
+
+ ### I have taken refernce from here:
+
+ https://www.youtube.com/watch?v=zhkhdD2hkQw&t (English)
+
+ https://www.youtube.com/watch?v=cgNQgcUgq0U&t (Hindi)
+
+ https://www.youtube.com/watch?v=lxvr7zgNMWY (Hindi)
+
+---
+### Disclaimer:
+
+ #### Use this code has an educational purpose only.<img src="https://i.ibb.co/6sKk5rS/8d34dccac3830875b9b7beeaddd39c34-w-1898-64.png" width="20" height="20" />
+
+
---
+++ educational purpose/For YouTube Creators - partial feature/app.py
@@ -0,0 +1,223 @@
+# Modules
+from selenium import  webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time , re, sys, xlsxwriter, argparse, validators, urllib
+
+# Youtube class
+class YouTube:
+    # Initial values
+    def __init__(self, url):
+        self.url = url
+        self.list = []
+        self.channelInfo = {}
+        self.count = 0
+        self.match = False
+
+    # Checking the youtube name only.
+    def check_url(self, url):
+        for x in url.split("/"):
+            if x.startswith("www.") or x.endswith(".com"):
+                print(x)
+                return x
+
+    # Checking the youtube domain only.
+    def matchYoutube(self, url):
+        array = self.check_url(url).split(".")
+        match = len(array)
+        count = 0
+        pattern = '^youtube$'
+        for x in array:
+            result = re.match(pattern, x.lower())
+            
+            if result:
+                return True
+            else:
+                if match == count:
+                    return False
+                else:
+                    count += 1
+
+    # Checking the channel URL it's has videos or not
+    def check_yt_link(self):
+        if validators.url(self.url):
+            if self.matchYoutube(self.url):
+                if self.url.endswith("/videos"):
+                    self.url = "{}".format(self.url)
+                elif "/watch?v=" in self.url:
+                    print("[-] This is a not channel link")
+                    sys.exit(1)  
+                elif self.url.endswith("/featured"):
+                     self.url = self.url.replace("featured","videos")
+                elif self.url.endswith("/playlists"):
+                     self.url = self.url.replace("playlists","videos")
+                elif self.url.endswith("/channels"):
+                     self.url = self.url.replace("channels","videos")
+                elif self.url.endswith("/about"):
+                     self.url = self.url.replace("about","videos")
+                elif self.url.endswith("/community"):
+                     self.url = self.url.replace("community","videos")
+                elif self.url.endswith("/store"):
+                     self.url = self.url.replace("store","videos")
+                elif self.url.endswith("/"):
+                    self.url  = "{}videos".format(self.url)                                    
+                else:
+                    self.url  = "{}/videos".format(self.url)
+                self.turl = self.url    
+                self.start()    
+            else:
+                print("[-] It must be youtube link")
+                sys.exit(1)
+        else:
+            print("[-] Not a valid link")
+            sys.exit(1)
+
+    # Getting the content from driver to covert into lxml  
+    def get_content_html(self, driver):
+        print("[+] gathering information of the channel videos")
+        content = driver.page_source.encode('utf-8').strip()
+        time.sleep(1)
+        soup = BeautifulSoup(content, 'lxml')
+        metaData = soup.findAll("a", id="video-title")
+        durations = soup.findAll("span", id="text")
+        count = 1
+        for meta, duration in zip(metaData, durations):
+            title = meta.get("title")
+            link = "https://www.youtube.com{}".format(meta.get("href"))
+            others = meta.get("aria-label")
+            duration = duration.get("aria-label")
+            views = others.split(" ")[-2]
+            isStreamed = ""
+            if "Streamed" in others:
+                isStreamed = "Streamed"   
+            else:
+                isStreamed = "Not Streamed"
+            gg = re.findall(r'(\d{1,}\s*(years|year|months|month|weeks|week|days|day|hours|hour|minutes|minuter|seconds|second))', others)
+            partialDate = ",".join([i[0] for i in gg]) 
+            data = {"no":count ,"title": title, "link": link, "views": views, "isStreamed": isStreamed, "date": partialDate, "duration": duration }
+            self.list.append(data)
+            count += 1
+        else:    
+            print("[+] Creating the file") 
+            self.createExcel()
+
+    # Creating the excel file
+    def createExcel(self):
+        fileName = "{}-videos-list.xlsx".format(self.channelInfo["name"])
+        workbook = xlsxwriter.Workbook(fileName)
+        worksheet = workbook.add_worksheet()
+
+        header = ["No of video", "Title", "Link", "Views", "isStreamed", "Relative time", "Duration"]
+        row = 0
+        col = 0
+        for i in header:
+            worksheet.write(row,col, i)
+            col += 1
+        row = 1
+        col = 0
+
+        for data in self.list:
+            worksheet.write(row, col, data["no"])
+            worksheet.write(row, col + 1, data["title"])
+            worksheet.write(row, col + 2, data["link"])
+            worksheet.write(row, col + 3, data["views"])
+            worksheet.write(row, col + 4, data["isStreamed"])
+            worksheet.write(row, col + 5, data["date"])
+            worksheet.write(row, col + 6, data["duration"])
+
+            row +=1
+        workbook.close()
+        print("[+] File Name is {}".format(fileName))
+
+    # Covert the url to proper format
+    def convert_yt_about_page_url(self, url):
+        return urllib.parse.unquote(url.split("q=")[-1])
+
+    # Channel about page
+    def get_about_the_channel(self, driver):
+        channelName = driver.find_element_by_css_selector("#channel-name")
+        channelJoindDate = driver.find_element_by_css_selector("#right-column > yt-formatted-string:nth-child(2)")
+        totalViews = driver.find_element_by_css_selector("#right-column > yt-formatted-string:nth-child(3)")
+        channelDescription = driver.find_element_by_css_selector("#description-container")
+        channelLocation = driver.find_element_by_css_selector("#details-container > table > tbody > tr:nth-child(2) > td:nth-child(2)")
+        channelSubscribe = driver.find_element_by_css_selector("#inner-header-container > #meta > yt-formatted-string").get_attribute('aria-label')
+        try:
+            channelBadge = driver.find_element_by_css_selector("#inner-header-container > #meta > #channel-name > ytd-badge-supported-renderer > div").get_attribute('aria-label')
+        except:
+            channelBadge = "Not Verified"
+        channelLinks = driver.find_element_by_css_selector("#link-list-container")
+        channelLinks = channelLinks.find_elements_by_tag_name("a")
+        channelLinks = [{"link": self.convert_yt_about_page_url(a.get_attribute("href")), "text": a.text} for a in channelLinks]
+        channelLinks =  "\n\n\t\t".join([str("{} : {}".format(i['text'], i['link'])) for i in channelLinks])
+        self.channelInfo  = {
+        "name":channelName.text,
+        "date":channelJoindDate.text,
+        "badge":channelBadge,
+        "total subscribers":channelSubscribe,
+        "views":totalViews.text,
+        "description":"\n\n=========================\n{}\n\n=========================".format(channelDescription.text),
+        "location":channelLocation.text,
+        "links": channelLinks
+        }
+
+    # Channel info text file
+    def createTxt(self):
+        lineTxt = [
+        "Channel URL : Nil \n\n",
+        "Channel ID : Nil \n\n",
+        "Adsense ID : Nil \n\n", 
+        "Email ID : Nil \n\n",
+        "Email createdAt : Nil \n\n",  
+        "Channel name : {} \n\n".format(self.channelInfo["name"]),
+        "Channel joined : {} \n\n".format(self.channelInfo["date"]),
+        "Channel badge status: {} \n\n".format(self.channelInfo["badge"]),
+        "Channel subscribers : {} \n\n".format(self.channelInfo["total subscribers"]),
+        "Channel total views : {} \n\n".format(self.channelInfo["views"]),
+        "Channel description : {} \n\n".format(self.channelInfo["description"]),
+        "Channel location : {} \n\n".format(self.channelInfo["location"]),
+        "Channel links : {} \n\n".format(self.channelInfo["links"])
+        ]
+        fileName = "{}-channel-info.txt".format(self.channelInfo["name"])
+        with open(fileName, "w", encoding="utf-8") as f:
+            f.writelines(lineTxt)
+        print("[+] channel info create {}".format(fileName))
+
+    # start the process
+    def start(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        driver.maximize_window()
+        print("[+] Getting info on channel...")
+        driver.get(self.url.replace("videos", "about"))
+        time.sleep(2)
+        self.get_about_the_channel(driver)
+        self.createTxt()
+        driver.get("{}?view=0&sort=da&flow=grid".format(self.url)) 
+
+        height = driver.execute_script("return document.documentElement.scrollHeight")
+        lastheight = 0
+        while (self.match == False):
+            if lastheight == height:
+                print("[+] scroll to the end")
+                self.match = True
+            lastheight = height
+            driver.execute_script("window.scrollTo(0, " + str(height) + ");")
+            time.sleep(2)
+            height = driver.execute_script("return document.documentElement.scrollHeight")
+        self.get_content_html(driver) 
+        print("[+] Done") 
+        driver.close() 
+
+    # Delete the initial values            
+    def __del__(self):
+        pass 
+
+parser = argparse.ArgumentParser(description="For creators")
+parser.add_argument("-l", "--link", required=True, help="Youtube channel link")
+args = vars(parser.parse_args())
+
+# checking the main class
+if __name__=="__main__":
+    start = YouTube(args["link"])
+    start.check_yt_link()
